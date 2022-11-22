419439044016049caada81445460391d0c8cb3e2
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-22 09:58:55 -0600

    Easy-HTTP2

--- app.js
+++ app.js
@@ -1,57 +1,28 @@
-const http = new easyHTTP;
+const http = new EasyHTTP;
 
-// Get Posts
+// Get Users
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(posts);
-//   }
-  
-// });
-
-// Get Single Post
-
-// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }  
-// });
-
-// Create Data
-
+// User Data
 const data = {
-  title: 'Custom Post',
-  body: 'This is a custom post'
-};
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'jdoe@gmail.com'
+}
 
-// Create Post
-// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
+//Create User
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-// Update Post
-http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
-  if(err) {
-        console.log(err);
-      } else {
-        console.log(post);
-      }
-});
+//Update User
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
 
-// Delete Post
-
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(response);
-  }  
-});
+//Delete User
+http.put('https://jsonplaceholder.typicode.com/users/2')
+.then(data => console.log(data))
+.catch(err => console.log(err));
